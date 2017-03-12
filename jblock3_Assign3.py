7aa4fdf3694ef8466465494d5d9814cd6ae10f11
jblock3 <block.jamie123@gmail.com>
2017-03-11 19:24:53 -0500

    initial commit

---
+++ jblock3_Assign3.py
@@ -0,0 +1,4 @@
+##
+# This program was coded by Jamie Block (Student #: 250777666)
+# This program is designed to analyze Twitter information using textual analysis
+# using functions, complex data structures, nested loops
