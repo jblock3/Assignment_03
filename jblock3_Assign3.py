04bbaada8deb90850d570208135ea704df57710f
jblock3 <block.jamie123@gmail.com>
2017-03-11 20:24:01 -0500

    started first try/except block

--- jblock3_Assign3.py
+++ jblock3_Assign3.py
@@ -2,3 +2,20 @@
 # This program was coded by Jamie Block (Student #: 250777666)
 # This program is designed to analyze Twitter information using textual analysis
 # using functions, complex data structures, nested loops
+#
+
+# Prompt user for name of file containing keywords; check if file exists
+
+try :
+    keywordsFile = input('Enter the filename that contains the keywords: ')
+    infile = open(keywordsFile, 'r')
+
+    unhappy = []    # For keywords with sentiment value of 1
+    satisfied = []  # For keywords with sentiment value of 5
+    happy = []      # For keywords with sentiment value of 10
+
+    for line in infile :
+        line = line.rstrip('\n')
+        print(line)
+finally :
+    print('bleh')
